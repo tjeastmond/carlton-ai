71e3ee0b9d220a6aa9ab9de196fee24e4a9a84c7
TJ Eastmond <tj.eastmond@gmail.com>
2024-11-07 17:38:16 -0500

    Initial Commit

---
+++ carlton/main.py
@@ -0,0 +1,135 @@
+import logging
+import os
+from datetime import datetime
+
+from openai import OpenAI
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.styles import Style
+from prompts import CREATE_SYSTEM_PROMPT
+from termcolor import colored
+
+# openai model
+MODEL = "gpt-4o-mini"
+
+# chat names
+BOT_NAME = "Carlton"
+USER_NAME = "You"
+
+# chat colors
+BOT_COLOR = "light_green"
+USER_COLOR = "light_blue"
+
+
+# create an OpenAI client
+client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
+
+# configure logging
+logging.basicConfig(filename="gen_code.log", level=logging.INFO, format="%(message)s")
+
+
+def clear_console():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def bot_asks() -> str:
+    user_input = input(f"{colored(USER_NAME, USER_COLOR)}: ").strip().lower()
+    return user_input
+
+
+def bot_says(message: str) -> None:
+    print(f"{colored(BOT_NAME, BOT_COLOR)}: {message}")
+
+
+def bot_listens(message: str) -> None:
+    bot_says(message)
+
+
+def generate_code(code_function):
+    prompt_string = CREATE_SYSTEM_PROMPT + f"\n\n{code_function}"
+
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "user", "content": prompt_string},
+        ],
+    )
+
+    code = response.choices[0].message.content
+
+    log_message(f"Prompt: {prompt_string}")
+    log_message(f"Generated code: {code}")
+
+    return code
+
+
+def log_message(message):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logging.info(f"{current_time} - {message}")
+
+
+def save_code_to_file(code):
+    user_input = input("Do you want to save this file? (y/n): ").strip().lower()
+
+    if user_input in ["y", "yes"]:
+        file_name = input("Please enter a file name: ").strip()
+        with open(file_name, "w") as file:
+            file.write(code)
+        print(f"Text saved to {file_name}.")
+
+    elif user_input in ["n", "no"]:
+        bot_says("Pft, fine")
+
+    else:
+        print("What? Try that again.")
+        save_code_to_file(code)
+
+
+def main():
+    try:
+        print()
+        clear_console()
+        bot_says("Ready to code")
+
+        completer = WordCompleter(
+            [
+                "/create",
+                "/quit",
+            ],
+            ignore_case=True,
+        )
+
+        style = Style.from_dict(
+            {
+                "prompt": "cyan",
+            }
+        )
+
+        while True:
+            user_input = (
+                prompt("You: ", completer=completer, style=style, multiline=True)
+                .strip()
+                .lower()
+            )
+
+            if user_input in ["bye", "/exit", "/quit", "exit", "quit"]:
+                bot_says("Later on!")
+                print()
+                break
+
+            elif user_input == "/create":
+                bot_says("Tell me what you are looking for")
+                response = bot_asks()
+                generated_code = generate_code(response)
+                print(f"\n{generated_code}\n")
+                save_code_to_file(generated_code)
+
+            else:
+                bot_says("I'm not sure what you mean")
+
+    except KeyboardInterrupt:
+        bot_says("Alright, see ya")
+
+
+if __name__ == "__main__":
+    main()
---
+++ carlton/prompts.py
@@ -0,0 +1,64 @@
+CREATE_SYSTEM_PROMPT = """
+You are an advanced o1 engineer designed to create files and folders based on user instructions. Your primary objective is to generate the content of the files to be created as code blocks. Each code block should specify whether it's a file or folder, along with its path.
+
+When given a user request, perform the following steps:
+
+1. Understand the User Request: Carefully interpret what the user wants to create.
+2. Generate Creation Instructions: Provide the content for each file to be created within appropriate code blocks. Each code block should begin with a special comment line that specifies whether it's a file or folder, along with its path.
+3. You create full functioning, complete,code files, not just snippets. No approximations or placeholders. FULL WORKING CODE.
+
+IMPORTANT: Your response must ONLY contain the code blocks with no additional text before or after. Do not use markdown formatting outside of the code blocks. Use the following format for the special comment line. Do not include any explanations, additional text:
+
+For folders:
+```
+### FOLDER: path/to/folder
+```
+
+For files:
+```language
+### FILE: path/to/file.extension
+File content goes here...
+```
+
+Example of the expected format:
+
+```
+### FOLDER: new_app
+```
+
+```html
+### FILE: new_app/index.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New App</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+</body>
+</html>
+```
+
+```css
+### FILE: new_app/styles.css
+body {
+    font-family: Arial, sans-serif;
+}
+```
+
+```javascript
+### FILE: new_app/script.js
+console.log('Hello, World!');
+```
+
+Ensure that each file and folder is correctly specified to facilitate seamless creation by the script.
+"""
+
+ROLE_PROMPT = """
+    You're an engineering expert who strives for the cleanest and most efficient code possible. You
+    are going to be asked to generate code by the user and you will reply only with the code that
+    satisfies the user's request. There will be no explanation or additional information provided.
+    You will exclude code fences, the responce should be in plain text. Never add comments.
+
+    If you cannot determine the programming languange to use, assume it is Python.
+"""

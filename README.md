36a49ad7af86645b0f22ddf1f5b14c0177331a6f
TJ Eastmond <tj.eastmond@gmail.com>
2024-11-14 12:30:54 -0500

    Reducing scope

---
+++ README.md
@@ -0,0 +1,16 @@
+# Carlton - A Simple Python AI Code Generator
+
+This project is a small exercise in code generation using Python via a command line interface.
+
+This project will currently only focus on working with single file creation, editing, reviewing, and refactoring. Maybe I will extend it t another time.
+
+## TODO
+
+- [x] Create a simple command line interface
+- [ ] Implement basic file creation
+- [ ] Implement editing command
+- [ ] Apply edits
+- [ ] Implement review command
+- [ ] Implement refactor command
+- [ ] Write code to apply edits
+- [ ] Write code to display review
--- carlton/main.py
+++ carlton/main.py
@@ -3,15 +3,16 @@ import os
 import sys
 from datetime import datetime
 
-import yaml
 from openai import OpenAI
-from prompts import BASIC_CREATE_PROMPT
+from prompts import BASIC_CREATE_PROMPT, CODE_REVIEW_PROMPT
+from rich import print as rprint
+from rich.markdown import Markdown
 from termcolor import colored
+
+ARROWS = colored(">>> ", "white")
 
 # openai model
 MODEL = "gpt-4o-mini"
-
-ARROWS = colored(">>> ", "white", attrs=["bold"])
 
 # create an OpenAI client
 client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
@@ -20,10 +21,40 @@ client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
 logging.basicConfig(filename="logs/gen_code.log", level=logging.INFO, format="%(message)s")
 
 
-def read_yaml_file(file_path):
-    with open(file_path, "r") as file:
-        data = yaml.safe_load(file)
-    return data
+def log_message(message: str) -> None:
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logging.info(f"{current_time} - {message}")
+
+
+def is_binary_file(file_path: str) -> bool:
+    if not os.path.isfile(file_path):
+        return False
+
+    with open(file_path, "rb") as f:
+        chunk = f.read(1024)
+        return b"\0" in chunk
+    return False
+
+
+def read_files(file_paths: list):
+    content = {}
+    for path in file_paths:
+        if os.path.isfile(path):
+            with open(path, "r") as file:
+                content[path] = file.read()
+        else:
+            content[path] = None
+    return content
+
+
+def create_context(file_paths):
+    file_contents = read_files(file_paths)
+    context = ""
+    for file in file_contents:
+        if file_contents[file] is not None:
+            context += f"### FILE: {file}" + "\n" + file_contents[file] + "\n"
+
+    return context
 
 
 def clear_console() -> None:
@@ -31,33 +62,26 @@ def clear_console() -> None:
 
 
 def generate_code(code_request):
-    prompt_string = BASIC_CREATE_PROMPT + f"\n\n{code_request}"
-
     response = client.chat.completions.create(
         model=MODEL,
         messages=[
-            {"role": "user", "content": prompt_string},
+            {"role": "user", "content": code_request},
         ],
     )
 
     code = response.choices[0].message.content
 
-    log_message(f"Prompt: {prompt_string}")
+    log_message(f"Prompt: {code_request}")
     log_message(f"Generated code: {code}")
 
     return code
-
-
-def log_message(message: str) -> None:
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    logging.info(f"{current_time} - {message}")
 
 
 def save_code_to_file(code: str) -> None:
     user_input = input(f"{ARROWS}Do you want to save this file? (y/n): ").strip().lower()
 
     if user_input in ["y", "yes"]:
-        file_name = input(f"{ARROWS}Please enter a file name: ").strip()
+        file_name = input(f"{ARROWS}Enter a file name: ").strip()
 
         with open(file_name, "w") as file:
             file.write(code)
@@ -72,12 +96,17 @@ def save_code_to_file(code: str) -> None:
         save_code_to_file(code)
 
 
-def chat_input() -> str:
-    response = input(f"{ARROWS}")
-    return response
+def save_to_file(file_name: str, code: str) -> None:
+    with open(file_name, "w") as file:
+        file.write(code)
+
+    print(colored(f"Text saved to {file_name}", "green"))
 
 
 def main() -> None:
+    last_response = None
+    cwd = os.getcwd()
+
     try:
         while True:
             user_input = input(f"{ARROWS}").strip().lower()
@@ -85,14 +114,73 @@ def main() -> None:
             if not user_input:
                 continue
 
-            elif user_input in ["bye", "/exit", "/quit"]:
-                print()
+            elif user_input.startswith("/create"):
+                user_instruction = user_input[7:].strip()
+
+                if not user_instruction:
+                    print(colored("You need to supply instructions", "red"))
+                    continue
+
+                prompt_string = BASIC_CREATE_PROMPT + f"{user_instruction}"
+                code = generate_code(prompt_string)
+                last_response = code
+
+                print(f"\n{code}\n")
+
+            elif user_input.startswith("/review"):
+                file_name = user_input[7:].strip()
+
+                if not file_name:
+                    print(colored("You need to supply a file name", "red"))
+                    continue
+
+                if is_binary_file(file_name):
+                    print(colored("Binary files are not supported", "red"))
+                    continue
+
+                file_path = f"{cwd}/{file_name}"
+                context = create_context([file_path])
+                prompt_string = CODE_REVIEW_PROMPT + context
+                review = generate_code(prompt_string)
+                last_response = review
+
+                print(prompt_string)
+
+                rprint(Markdown(review))
+
+            elif user_input.startswith("/save"):
+                if not last_response:
+                    print(colored("No code to save", "red"))
+                    continue
+
+                file_name = user_input[5:].strip()
+
+                if not file_name:
+                    print(colored("You need to supply a file name", "red"))
+                    continue
+
+                save_to_file(f"{cwd}/{file_name}", last_response)
+
+            elif user_input in ["/edit"]:
+                pass
+
+            elif user_input in ["/exit"]:
                 break
 
+            elif user_input == "/?":
+                help_text = ""
+                help_text += colored("\nSlash Commands:\n\n", "green")
+                help_text += colored("  /create", "yellow") + colored(" - Create a new file\n", "light_grey")
+                help_text += colored("  /edit  ", "yellow") + colored(" - Edit a file. File path required\n", "light_grey")
+                help_text += colored("  /review", "yellow") + colored(" - Give me a file name and I will review it\n", "light_grey")
+                help_text += colored("  /save  ", "yellow") + colored(" - Supply a file name to save your work\n", "light_grey")
+                help_text += colored("  /exit  ", "yellow") + colored(" - Exit the program\n", "light_grey")
+
+                print(help_text)
+
             else:
-                generated_code = generate_code(user_input)
-                print(f"\n{generated_code}\n")
-                save_code_to_file(generated_code)
+                print(colored("Invalid command", "red"))
+                pass
 
     except KeyboardInterrupt:
         sys.stdout.write("\r")
